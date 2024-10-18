9b01e2d6e121ab7043809cdc38842a20ff9b4bc3
jsburckhardt <jsburckhardt@gmail.com>
2024-10-18 04:09:44 +0000

    chore(tests): update kyverno and zarf versions in tests
    - Updated kyverno version from 1.11.4 to 1.12.6 in kyverno-specific-version.sh and scenarios.json
    - Updated zarf version from v0.30.0 to v0.41.0 in zarf-specific-version.sh and scenarios.json

--- test/_global/kyverno-specific-version.sh
+++ test/_global/kyverno-specific-version.sh
@@ -2,6 +2,6 @@
 
 set -e
 source dev-container-features-test-lib
-check "kyverno with specific version" /bin/bash -c "kyverno version | grep '1.11.4'"
+check "kyverno with specific version" /bin/bash -c "kyverno version | grep '1.12.6'"
 
 reportResults
