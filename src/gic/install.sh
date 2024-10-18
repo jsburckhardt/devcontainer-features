aa04b5b8080173fab2a557ee867465410a55e9bb
jsburckhardt <jsburckhardt@gmail.com>
2024-10-18 05:20:30 +0000

    fix(install): correct indentation and improve version check logic
    Corrected the indentation in the install.sh script for better readability and maintainability. Improved the version check logic to handle environment variable inputs more robustly. The script now properly fetches the latest version if 'latest' is specified or if no version is provided.

--- src/gic/install.sh
+++ src/gic/install.sh
@@ -8,27 +8,25 @@ BINARY_NAME="gic"
 set -e
 
 if [ "$(id -u)" -ne 0 ]; then
-	echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
-	exit 1
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
 fi
 
 # Clean up
 rm -rf /var/lib/apt/lists/*
 
 check_packages() {
-	if ! dpkg -s "$@" >/dev/null 2>&1; then
-		if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-			echo "Running apt-get update..."
-			apt-get update -y
-		fi
-		apt-get -y install --no-install-recommends "$@"
-	fi
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt-get update..."
+            apt-get update -y
+        fi
+        apt-get -y install --no-install-recommends "$@"
+    fi
 }
-
 
 # make sure we have packages
 check_packages curl tar jq ca-certificates
-
 
 # Function to get the latest version from GitHub API
 get_latest_version() {
@@ -36,14 +34,13 @@ get_latest_version() {
     curl -s "$LATEST_URL" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
 }
 
-# Check if a version is passed as an argument
-if [ -z "$1" ]; then
-    # No version provided, get the latest version
+# Check if a version is provided via environment variable or passed as an argument
+if [ -z "$VERSION" ] || [ "$VERSION" == "latest" ]; then
     VERSION=$(get_latest_version)
-    echo "No version provided, installing the latest version: $VERSION"
+    echo "No version provided or 'latest' specified, installing the latest version: $VERSION"
 else
-    VERSION=$1
-    echo "Installing specified version: $VERSION"
+    # trim the v from the version
+    echo "Installing version from environment variable: $VERSION"
 fi
 
 # Determine the OS and architecture following the naming template
