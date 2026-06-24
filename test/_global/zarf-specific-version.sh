df8eaf808f3cf9a159d5b393d5cf04497ae43dbe
jsburckhardt <jsburckhardt@gmail.com>
2026-06-24 09:40:26 +0000

    feat: update zarf version to v0.79.0 in scenarios and test script

--- test/_global/zarf-specific-version.sh
+++ test/_global/zarf-specific-version.sh
@@ -2,6 +2,6 @@
 
 set -e
 source dev-container-features-test-lib
-check "zarf with specific version" /bin/bash -c "zarf version | grep 'v0.61.2'"
+check "zarf with specific version" /bin/bash -c "zarf version | grep 'v0.79.0'"
 
 reportResults
